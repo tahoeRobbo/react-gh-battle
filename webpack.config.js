f1a1671deb31a442c0512ac02823bd0f78bb508a
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-08-20 18:53:31 -0500

    finished setting up babel webpack and package.json, first hello world component. build and start commands work

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    module: {
+        rules: [
+            { test: /\.svg$/, use: 'svg-inline-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(js)$/, use: 'babel-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
