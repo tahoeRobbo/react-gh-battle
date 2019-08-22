53cd777fecde7fb526a71b9ab7fb60c52def3b58
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-08-21 22:43:28 -0500

    basics of state and styles depending on state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage (selectedLanguage) {
+        this.setState({
+            selectedLanguage: selectedLanguage
+        })
+    }
+
     render() {
         const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 
@@ -8,7 +24,12 @@ export default class Popular extends React.Component {
             <ul className='flex-center'>
                 {languages.map((language, i) =>
                     <li key={i}>
-                        <button className='btn-clear nav-link'>{language}</button>
+                        <button
+                            className='btn-clear nav-link'
+                            style={language === this.state.selectedLanguage ? {color: 'rgb(69, 210, 99)'} : null}
+                            onClick={() => this.updateLanguage(language)}>
+                            {language}
+                        </button>
                     </li>
                 )}
             </ul>
