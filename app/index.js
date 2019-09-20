0b92c219813fb4587243cf7f9500cbdd1407fe57
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-09-20 16:35:06 -0500

    basic code splitting with React.lazy around the specific routes

--- app/index.js
+++ app/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Hello from "./components";
-import Popular from "./components/Popular";
-import Battle from "./components/Battle";
-import Results from "./components/Results";
+// import Popular from "./components/Popular";
+// import Battle from "./components/Battle";
+// import Results from "./components/Results";
 import Nav from "./components/Nav";
+import Loading from "./components/Loading";
 
 import { ThemeProvider } from "./contexts/Theme";
 
 import './index.css'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends React.Component {
     // new Class Fields way.  Currently in stage 3 spec and popular in the community.
@@ -49,13 +53,15 @@ class App extends React.Component {
                     <div className={this.state.theme}>
                         <div className="container">
                             <Nav />
-                            <Switch>
-                                <Route exact path='/' component={Popular} />
-                                <Route exact path='/battle' component={Battle} />
-                                <Route path='/battle/results' component={Results} />
-                                {/*<Route component={FourOhFour} /> TODO implement a 404 component? */}
-                                <Route render={() => <h1>404</h1>} />
-                            </Switch>
+                            <React.Suspense fallback={<Loading />}>
+                                <Switch>
+                                    <Route exact path='/' component={Popular} />
+                                    <Route exact path='/battle' component={Battle} />
+                                    <Route path='/battle/results' component={Results} />
+                                    {/*<Route component={FourOhFour} /> TODO implement a 404 component? */}
+                                    <Route render={() => <h1>404</h1>} />
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
