cd24b46c7d95165977e9b10d2e1ece35009d1d88
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-05 13:03:06 -0600

    removed eslint from package, minor clearup on useHover, added packagelock and .idea to gitignore

--- app/hooks/useHover.js
+++ app/hooks/useHover.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function useHover () {
-  const [ hovering, setHovering ] = React.useState(false)
+  const [hovering, setHovering] = React.useState(false)
 
   const onMouseOver = () => setHovering(true)
   const onMouseOut = () => setHovering(false)
 
-  const attrs= {
+  const attrs = {
     onMouseOver,
     onMouseOut
   }
